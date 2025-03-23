08b45f69fdbca051fb0e45491607a20106fb2517
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2025-03-22 19:39:38 -0700

    bullet points work, made border width consistent on window scaling

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   main: {
@@ -20,6 +20,9 @@ export default defineConfig({
     plugins: [
       react(),
       tailwindcss(),
+      svgr({
+        exportAs: 'ReactComponent'
+      }),
     ]
   }
 })
--- src/renderer/src/components/StickyNote.jsx
+++ src/renderer/src/components/StickyNote.jsx
@@ -1,9 +1,14 @@
 import { useState, useRef, useEffect } from 'react';
-
+import StrikeThroughIcon from '../../../../resources/icons/regular/strike-through.svg?react';
+import UnderlineIcon from '../../../../resources/icons/regular/Underline.svg?react';
+import BoldIcon from '../../../../resources/icons/regular/Bold.svg?react';
+import ItalicsIcon from '../../../../resources/icons/regular/italics.svg?react';
+import WindowCloseIcon from '../../../../resources/icons/regular/window-close.svg?react';
+import BulletIcon from '../../../../resources/icons/regular/bullet-list.svg?react';
+import DividerIcon from '../../../../resources/icons/regular/divider.svg?react';
 function StickyNote(){
     const [content, setContent] = useState('');
     const editorRef = useRef(null);
-
         const handleInput = () => {
             const html = editorRef.current.innerHTML;
             lastHtml.current = html;
@@ -42,7 +47,7 @@ function StickyNote(){
                     <div className = "grid grid-cols-2 grid-rows-1 min-w-full">
                         <div className = "flex float-left">
                             <p id="not-draggable"><b>+</b></p>
-                            
+
                         </div>
                         <div className = "flex justify-end gap-3">
                             <p onClick = {()=>minimizeWindow()} id="not-draggable"><b>-</b></p>
@@ -52,8 +57,8 @@ function StickyNote(){
                 </div>
 
                 {/* //Text Area */}
-                <div className="flex w-[96%] min-h-[65%] mx-auto overflow-x-hidden">
-                        <div className="flex bg-[#FFF5F7] min-h-auto min-w-full rounded-t-md p-2 ">
+                <div className="flex w-full min-h-[65%] overflow-x-hidden px-1">
+                        <div className="flex bg-[#FFF5F7] min-h-auto w-full rounded-t-md p-2 mx-auto max-w-[calc(100%-8px)]">
                             <div
                             ref={editorRef}
                             className="min-h-full min-w-full outline-none"
@@ -67,22 +72,21 @@ function StickyNote(){
                 </div>
 
                 {/* //Footer */}
-                <div className="flex justify-center items-center w-[96%] min-h-[10%] mt-1 mb-3 mx-auto">
-                    <div className="flex bg-[#FFF5F7] min-h-full min-w-full rounded-b-md p-2 ">
-                    <div className = "grid grid-cols-2 grid-rows-1 min-w-full">
-                        <div className = "flex float-left gap-1">
-
-                            <button onClick={() => formatText('bold')}>B</button>
-                            <button onClick={() => formatText('italic')}><i>I</i></button>
-                            <button onClick={() => formatText('underline')}>U</button>
-                            <button onClick={() => formatText('strikeThrough')}>abc</button>
-                            <button onClick={() => formatText('insertUnorderedList')}>•</button>
-                            <button onClick={() => formatText('insertHorizontalRule')}>=</button>
+                <div className="flex justify-center items-center w-full min-h-[10%] mt-1 mb-3 px-1">
+                    <div className="flex bg-[#FFF5F7] min-h-full w-full rounded-b-md p-2 mx-auto max-w-[calc(100%-8px)]">
+                        <div className = "grid grid-cols-2 min-w-full">
+                            <div className = "grid grid-cols-7 gap-8">
+                                <button onClick={() => formatText('bold')} className="flex justify-center items-center min-w-6 min-h-6 hover:bg-gray-400 rounded-sm"><BoldIcon className="w-4 h-4"/></button>
+                                <button onClick={() => formatText('italic')} className="flex justify-center items-center min-w-6 min-h-6 hover:bg-gray-400 rounded-sm"><ItalicsIcon className="w-4 h-4"/></button>
+                                <button onClick={() => formatText('underline')} className="flex justify-center items-center min-w-6 min-h-6 hover:bg-gray-400 rounded-sm"><UnderlineIcon className="w-4 h-4"/></button>
+                                <button onClick={() => formatText('strikeThrough')} className="flex justify-center items-center min-w-6 min-h-6 hover:bg-gray-400 rounded-sm"><StrikeThroughIcon className="w-4 h-4"/></button>
+                                <button onClick={() => formatText('insertUnorderedList')} className="flex justify-center items-center min-w-6 min-h-6 hover:bg-gray-400 rounded-sm"><BulletIcon className="w-4 h-4"/></button>
+                                <button onClick={() => formatText('insertHorizontalRule')} className="flex justify-center items-center min-w-6 min-h-6 hover:bg-gray-400 rounded-sm"><DividerIcon className="w-4 h-4"/></button>
+                            </div>
+                            <div className="flex justify-end items-center w-full">
+                                <div className = "flex justify-center align-middle min-w-6 min-h-6 hover:bg-gray-400 rounded-sm"><p>//</p></div>
+                            </div>
                         </div>
-                        <div className = "flex justify-end gap-3">
-                            <p>//</p>
-                        </div>
-                    </div>
                     </div>
                 </div>
             </div>
