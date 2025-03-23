439b1db502b314c73d141b40d82d076dab586445
PhuNN16 <phunick316@gmail.com>
2025-03-23 06:50:39 -0700

    sticky folder UI

--- src/renderer/src/components/StickyFolder.jsx
+++ src/renderer/src/components/StickyFolder.jsx
@@ -24,21 +24,23 @@ function StickyFolder(id, name) {
     }
 
     return (
-        <div className="flex flex-col h-screen bg-red-400">
-            {/* Scrollable Content Area */}
-            <div 
-                className="flex-1 overflow-y-auto p-2 space-y-2"
-                style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}
-                >
-                {/* Dynamic Bookmarks */}
-                {stickynotes.map((stickynote) => (
-                <StickyNoteIcon
-                    key={stickynote.id}
-                    id={stickynote.id}
-                    name={stickynote.name}
-                    onDelete={deleteStickyNote}
-                />
-                ))}
+        <div className="flex bg-[#F8EAA6] w-screen h-screen justify-center align-middle items-center p-1">
+            <div className="flex flex-col w-full max-w-[100% - 4px] h-full max-h-[100% - 4px]  bg-[#F8EAA6] p-[2px]">
+                {/* Scrollable Content Area */}
+                <div 
+                    className="flex-1 overflow-y-auto p-2 space-y-2"
+                    style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}
+                    >
+                    {/* Dynamic Bookmarks */}
+                    {stickynotes.map((stickynote) => (
+                    <StickyNoteIcon
+                        key={stickynote.id}
+                        id={stickynote.id}
+                        name={stickynote.name}
+                        onDelete={deleteStickyNote}
+                    />
+                    ))}
+                </div>
             </div>
         </div>
     )
--- src/renderer/src/components/StickyNoteIcon.jsx
+++ src/renderer/src/components/StickyNoteIcon.jsx
@@ -1,6 +1,8 @@
 function StickyNoteIcon({id, name, onDelete}) {
     return (
-        <button onClick={() => {onDelete(id)}}> STICK </button>
+        <div className="grid grid-cols-2 gap-2 p-2 bg-[#FFFAE4] hover:bg-gray-100">
+            <button onClick={() => {onDelete(id)}}> {name} </button>
+        </div>
     )
 }
 
