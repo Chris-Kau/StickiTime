a54755f3682aad72bc18db2a788fd7d510ef0849
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2025-03-23 07:42:05 -0700

    hover animation

--- src/renderer/src/components/StickyNoteIcon.jsx
+++ src/renderer/src/components/StickyNoteIcon.jsx
@@ -1,7 +1,7 @@
 import Icon from '../../../../resources/icons/regular/receipt.svg?react';
 function StickyNoteIcon({id, name, onDelete}) {
     return (
-        <div onClick={() => {onDelete(id)}} className="flex min-w-[100%] justify-center mx-auto align-middle items-center hover:bg-gray-100/30 rounded-sm">
+        <div onClick={() => {onDelete(id)}} className="flex min-w-[100%] justify-center mx-auto align-middle items-center transition-all duration-[25ms] hover:scale-105">
             <Icon/>
         </div>
     )
